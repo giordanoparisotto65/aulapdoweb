954d61d330b5d057bfd6699193f2c0142e85b77c
giordanoparisotto65 <114354902+giordanoparisotto65@users.noreply.github.com>
2023-03-24 15:18:29 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <a href="./view/UsuarioForm.php">Cadastrar </a><br>
+    <a href="./view/UsuarioList.php">Listar </a>
+	</body>
+</html>
